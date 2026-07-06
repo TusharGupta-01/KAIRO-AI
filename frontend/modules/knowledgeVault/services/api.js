f69e5266c33e5ce9fb23d85cb6ff1a78f04f3031
Tushar Gupta <tushargupta17nov2005@gmail.com>
2026-07-06 19:33:22 +0530

    Change api file

--- frontend/modules/knowledgeVault/services/api.js
+++ frontend/modules/knowledgeVault/services/api.js
@@ -1,52 +1,14 @@
-import axios from "axios";
-const api = axios.create({
-  baseURL: `http://${API}/api`,
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
+import api from "../../../src/services/api";
 
 export const folderAPI = {
   getAll: () => api.get("/knowledge-vault/folders"),
 
-  create: (data) => api.post("/knowledge-vault/folders", data),
+  create: (data) =>
+    api.post("/knowledge-vault/folders", data),
 
   update: (id, data) =>
     api.put(`/knowledge-vault/folders/${id}`, data),
 
   remove: (id) =>
     api.delete(`/knowledge-vault/folders/${id}`),
-};
-
-export default api;
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
-
-export const folderAPI = {
-  getAll: () => api.get("/knowledge-vault/folders"),
-
-  create: (data) => api.post("/knowledge-vault/folders", data),
-
-  update: (id, data) =>
-    api.put(`/knowledge-vault/folders/${id}`, data),
-
-  remove: (id) =>
-    api.delete(`/knowledge-vault/folders/${id}`),
-};
-
-export default api;
+};  
